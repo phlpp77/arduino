5aaf29f480dfdba432dfaa44a4de8a02cc87d8e8
phlpp77 <PhilippHemkemeyer@iCloud.com>
2023-02-01 10:04:14 -0500

    Add dimmable LED

--- analog-write/analog-write.ino
+++ analog-write/analog-write.ino
@@ -1,12 +1,21 @@
 #define ledPin 11
 
 void setup() {
-  // put your setup code here, to run once:
-pinMode(ledPin, OUTPUT);
+  // define pin as output for LED
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  analogWrite(ledPin, 255);
-  // put your main code here, to run repeatedly:
 
+// light LED up (controlled by step count or delay)
+  for (int lightValue = 0; lightValue <= 255; lightValue += 50) {
+    analogWrite(ledPin, lightValue);
+    delay(30);
+  }
+
+// dimm LED down (controlled by step count or delay)
+  for (int lightValue = 255; lightValue >= 0; lightValue -= 50) {
+    analogWrite(ledPin, lightValue);
+    delay(30);
+  }
 }
