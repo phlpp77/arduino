62abcd93ce7acf4d37396f7a2a606b5d1b4d9e18
phlpp77 <PhilippHemkemeyer@iCloud.com>
2023-02-04 18:56:14 -0500

    Assignment completed

--- assignment-1-sensors-motors/assignment-1-sensors-motors.ino
+++ assignment-1-sensors-motors/assignment-1-sensors-motors.ino
@@ -1,3 +1,6 @@
+// Include the servo library to interact with servo
+#include <Servo.h>  // Servo library can now be used
+
 // Define all LED pins
 #define greenLedPin 13   // Green LED is pin 13
 #define yellowLedPin 12  // Yellow LED is pin 12
@@ -9,6 +12,9 @@
 // Define variables for if statements
 int buttonState = 0;
 
+// Create servo object to use later
+Servo myServo;
+
 void setup() {
   // Set pinmodes of LED pins to output
   pinMode(greenLedPin, OUTPUT);
@@ -17,19 +23,31 @@ void setup() {
 
   // Set pinmode of button pin to input
   pinMode(buttonPin, INPUT);
+
+  // Setup servo at pin 6 (needs to support PWM)
+  myServo.attach(6);
 }
 
 void loop() {
+
+  // Check if button is pressed buttonState == 1 or not == 0
   buttonState = digitalRead(buttonPin);
+
+  // If the button is pressed (buttonState ==1) do everything inside {}
   if (buttonState == 1) {
+
+    // Flash the LED 3 times, so the code inside the for {} is execuded 3 times
     for (int i = 0; i <= 2; i++) {
-      digitalWrite(greenLedPin, HIGH);
-      delay(500);  // Wait for 1000 millisecond(s)
-      digitalWrite(greenLedPin, LOW);
-      delay(500);  // Wait for 1000 millisecond(s)
+      digitalWrite(greenLedPin, HIGH);  // Green on
+      delay(500);
+      digitalWrite(greenLedPin, LOW);  // Green off
+      delay(500);
     }
     // Pause for 3s (3000ms)
     delay(3000);
+
+    // Rotate servo by 90deg
+    myServo.write(-90);
 
     // Flash LEDs from right to left
     digitalWrite(redLedPin, HIGH);  // Red on
@@ -50,6 +68,9 @@ void loop() {
     // Pause 5s (5000ms)
     delay(5000);
 
+    // Reverse servo by 90deg
+    myServo.write(90);
+
     // Flash LEDs from left to right
     digitalWrite(greenLedPin, HIGH);  // Green on
     delay(500);
@@ -65,6 +86,5 @@ void loop() {
     delay(500);
     digitalWrite(redLedPin, LOW);  // Red off
     delay(500);
-    
   }
 }
