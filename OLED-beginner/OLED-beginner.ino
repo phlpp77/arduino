91904cde6cffdc219444962bffe84c48d6303687
phlpp77 <PhilippHemkemeyer@iCloud.com>
2023-03-13 10:41:17 -0400

    Change names and ass2 working

--- OLED-beginner/OLED-beginner.ino
+++ OLED-beginner/OLED-beginner.ino
@@ -1,24 +1,3 @@
-/**************************************************************************
- This is an example for our Monochrome OLEDs based on SSD1306 drivers
-
- Pick one up today in the adafruit shop!
- ------> http://www.adafruit.com/category/63_98
-
- This example is for a 128x64 pixel display using I2C to communicate
- 3 pins are required to interface (two I2C and one reset).
-
- Adafruit invests time and resources providing this open
- source code, please support Adafruit and open-source
- hardware by purchasing products from Adafruit!
-
- Written by Limor Fried/Ladyada for Adafruit Industries,
- with contributions from the open source community.
- BSD license, check license.txt for more information
- All text above, and the splash screen below must be
- included in any redistribution.
- **************************************************************************/
-
-#include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
@@ -31,23 +10,12 @@
 // On an arduino UNO:       A4(SDA), A5(SCL)
 // On an arduino MEGA 2560: 20(SDA), 21(SCL)
 // On an arduino LEONARDO:   2(SDA),  3(SCL), ...
-#define OLED_RESET 4         // Reset pin # (or -1 if sharing Arduino reset pin)
+#define OLED_RESET -1        // Reset pin # (or -1 if sharing Arduino reset pin)
 #define SCREEN_ADDRESS 0x3D  ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-#define NUMFLAKES 10  // Number of snowflakes in the animation example
-
 #define LOGO_HEIGHT 16
 #define LOGO_WIDTH 16
-
-// Setup for buttons
-#define BUTTON_PIN_YELLOW 3
-#define BUTTON_PIN_RED 2
-int yellowButtonState = 0;
-int redButtonState = 0;
-
-// Menu status
-int menuSelection = 0;
 
 static const unsigned char PROGMEM logo_bmp[] = { 0b00000000, 0b11000000,
                                                   0b00000001, 0b11000000,
@@ -66,7 +34,13 @@ static const unsigned char PROGMEM logo_bmp[] = { 0b00000000, 0b11000000,
                                                   0b01110000, 0b01110000,
                                                   0b00000000, 0b00110000 };
 
+
+
+// [TODO] Define your global variables here 
+
+
 void setup() {
+  // put your setup code here, to run once:
   Serial.begin(9600);
 
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
@@ -76,411 +50,45 @@ void setup() {
       ;  // Don't proceed, loop forever
   }
 
-  // Initialize button pins
-  pinMode(BUTTON_PIN_YELLOW, INPUT);
-  pinMode(BUTTON_PIN_RED, INPUT);
+  // [TODO] Configure pinMode() here
 
-  // Show initial display buffer contents on the screen --
-  // the library initializes this with an Adafruit splash screen.
-  display.display();
+
+
   delay(2000);  // Pause for 2 seconds
 
-  // Clear the buffer
-  display.clearDisplay();
+  // Calls the first screen. Everytime you reset or switch 
+  // on the device this function is called.
+  first();
+  delay(4000);  // Pause for 4 seconds
+  
+  // [TODO] Call function here to display Menu screen with selection bar sitting on "1. Start"
 
-  showHelloText();  // Show startup text
 
-  showMenu();  // Show the menu
-
-  // testdrawline();  // Draw many lines
-
-  // testdrawrect();  // Draw rectangles (outlines)
-
-  // testfillrect();  // Draw rectangles (filled)
-
-  // testdrawcircle();  // Draw circles (outlines)
-
-  // testfillcircle();  // Draw circles (filled)
-
-  // testdrawroundrect();  // Draw rounded rectangles (outlines)
-
-  // testfillroundrect();  // Draw rounded rectangles (filled)
-
-  // testdrawtriangle();  // Draw triangles (outlines)
-
-  // testfilltriangle();  // Draw triangles (filled)
-
-  // testdrawchar();  // Draw characters of the default font
-
-  // testdrawstyles();  // Draw 'stylized' characters
-
-  // testscrolltext();  // Draw scrolling text
-
-  // testdrawbitmap();  // Draw a small bitmap image
-
-  // Invert and restore display, pausing in-between
-  // display.invertDisplay(true);
-  // delay(1000);
-  // display.invertDisplay(false);
-  // delay(1000);
-
-  // testanimate(logo_bmp, LOGO_WIDTH, LOGO_HEIGHT);  // Animate bitmaps
+  delay(100);
 }
 
 void loop() {
+  // put your main code here, to run repeatedly:
 
-  yellowButtonState = digitalRead(BUTTON_PIN_YELLOW);
-  redButtonState = digitalRead(BUTTON_PIN_RED);
-  // Serial.println(yellowButtonState);
-  // Serial.println(redButtonState);
-  // delay(500);
-  // Check if button is pressed and menu is switched
-  if (yellowButtonState == 1) {
-    menuSelection += 1;
-    if (menuSelection == 2) {
-      menuSelection = 0;  // if last position (2) is reached go back to first
-    }
-  }
-  Serial.print("Menu: ");
-  Serial.println(menuSelection);
-  switch (menuSelection) {
-    case 0:
-      display.fillRect(0, 0, 128, 16, SSD1306_INVERSE);
-    case 1:
-      display.fillRect(0, 17, 128, 16, SSD1306_INVERSE);
-    case 2:
-      display.fillRect(0, 34, 128, 16, SSD1306_INVERSE);
-  }
+  // [TODO] Read "Down" Button's current state.
+  // If pressed -> Perform relevant action.
+  // Hint: Go through Lecture 3 slides. We have discussed this in the "Tasks" section.  
 
-  display.display();
-}
+  
 
-void showHelloText(void) {
-  display.clearDisplay();
-  display.setTextSize(3);  // Draw 3X-scale text
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(10, 0);
-  display.println("Hello");
-  display.display();  // Show text on display
-  delay(1000);        // TODO change back to 4000
-}
-
-void showMenu(void) {
-  display.clearDisplay();
-  display.setTextSize(2);  // Draw 3X-scale text
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(10, 0);
-  display.println("1. Start");
-  display.setCursor(10, 17);
-  display.println("2. Stats");
-  display.setCursor(10, 34);
-  display.println("3. End");
-
-
-
-  // for (int16_t i = 0; i < display.height() / 2; i += 3) {
-  //   // The INVERSE color is used so rectangles alternate white/black
-  //   display.fillRect(i, i, display.width() - i * 2, display.height() - i * 2, SSD1306_INVERSE);
-  //   display.display();  // Update screen with each newly-drawn rectangle
-  //   delay(1);
-  // }
-
-  display.display();  // Show text on display
-}
-
-void testdrawline() {
-  int16_t i;
-
-  display.clearDisplay();  // Clear display buffer
-
-  for (i = 0; i < display.width(); i += 4) {
-    display.drawLine(0, 0, i, display.height() - 1, SSD1306_WHITE);
-    display.display();  // Update screen with each newly-drawn line
-    delay(1);
-  }
-  for (i = 0; i < display.height(); i += 4) {
-    display.drawLine(0, 0, display.width() - 1, i, SSD1306_WHITE);
-    display.display();
-    delay(1);
-  }
-  delay(250);
-
-  display.clearDisplay();
-
-  for (i = 0; i < display.width(); i += 4) {
-    display.drawLine(0, display.height() - 1, i, 0, SSD1306_WHITE);
-    display.display();
-    delay(1);
-  }
-  for (i = display.height() - 1; i >= 0; i -= 4) {
-    display.drawLine(0, display.height() - 1, display.width() - 1, i, SSD1306_WHITE);
-    display.display();
-    delay(1);
-  }
-  delay(250);
-
-  display.clearDisplay();
-
-  for (i = display.width() - 1; i >= 0; i -= 4) {
-    display.drawLine(display.width() - 1, display.height() - 1, i, 0, SSD1306_WHITE);
-    display.display();
-    delay(1);
-  }
-  for (i = display.height() - 1; i >= 0; i -= 4) {
-    display.drawLine(display.width() - 1, display.height() - 1, 0, i, SSD1306_WHITE);
-    display.display();
-    delay(1);
-  }
-  delay(250);
-
-  display.clearDisplay();
-
-  for (i = 0; i < display.height(); i += 4) {
-    display.drawLine(display.width() - 1, 0, 0, i, SSD1306_WHITE);
-    display.display();
-    delay(1);
-  }
-  for (i = 0; i < display.width(); i += 4) {
-    display.drawLine(display.width() - 1, 0, i, display.height() - 1, SSD1306_WHITE);
-    display.display();
-    delay(1);
-  }
-
-  delay(2000);  // Pause for 2 seconds
-}
-
-void testdrawrect(void) {
-  display.clearDisplay();
-
-  display.setTextSize(1);
-  display.setCursor(10, 10);
-  display.println("Hello there!");
-  // display.drawRect(0, 15, 30, 30);
-  display.display();
-
-  // for(int16_t i=0; i<display.height()/2; i+=2) {
-  //   display.drawRect(i, i, display.width()-2*i, display.height()-2*i, SSD1306_WHITE);
-  //   display.display(); // Update screen with each newly-drawn rectangle
-  //   delay(1);
-  // }
-
-  delay(2000);
-}
-
-void testfillrect(void) {
-  display.clearDisplay();
-
-  for (int16_t i = 0; i < display.height() / 2; i += 3) {
-    // The INVERSE color is used so rectangles alternate white/black
-    display.fillRect(i, i, display.width() - i * 2, display.height() - i * 2, SSD1306_INVERSE);
-    display.display();  // Update screen with each newly-drawn rectangle
-    delay(1);
-  }
-
-  delay(2000);
-}
-
-void testdrawcircle(void) {
-  display.clearDisplay();
-
-  for (int16_t i = 0; i < max(display.width(), display.height()) / 2; i += 2) {
-    display.drawCircle(display.width() / 2, display.height() / 2, i, SSD1306_WHITE);
-    display.display();
-    delay(1);
-  }
-
-  delay(2000);
-}
-
-void testfillcircle(void) {
-  display.clearDisplay();
-
-  for (int16_t i = max(display.width(), display.height()) / 2; i > 0; i -= 3) {
-    // The INVERSE color is used so circles alternate white/black
-    display.fillCircle(display.width() / 2, display.height() / 2, i, SSD1306_INVERSE);
-    display.display();  // Update screen with each newly-drawn circle
-    delay(1);
-  }
-
-  delay(2000);
-}
-
-void testdrawroundrect(void) {
-  display.clearDisplay();
-
-  for (int16_t i = 0; i < display.height() / 2 - 2; i += 2) {
-    display.drawRoundRect(i, i, display.width() - 2 * i, display.height() - 2 * i,
-                          display.height() / 4, SSD1306_WHITE);
-    display.display();
-    delay(1);
-  }
-
-  delay(2000);
-}
-
-void testfillroundrect(void) {
-  display.clearDisplay();
-
-  for (int16_t i = 0; i < display.height() / 2 - 2; i += 2) {
-    // The INVERSE color is used so round-rects alternate white/black
-    display.fillRoundRect(i, i, display.width() - 2 * i, display.height() - 2 * i,
-                          display.height() / 4, SSD1306_INVERSE);
-    display.display();
-    delay(1);
-  }
-
-  delay(2000);
-}
-
-void testdrawtriangle(void) {
-  display.clearDisplay();
-
-  for (int16_t i = 0; i < max(display.width(), display.height()) / 2; i += 5) {
-    display.drawTriangle(
-      display.width() / 2, display.height() / 2 - i,
-      display.width() / 2 - i, display.height() / 2 + i,
-      display.width() / 2 + i, display.height() / 2 + i, SSD1306_WHITE);
-    display.display();
-    delay(1);
-  }
-
-  delay(2000);
-}
-
-void testfilltriangle(void) {
-  display.clearDisplay();
-
-  for (int16_t i = max(display.width(), display.height()) / 2; i > 0; i -= 5) {
-    // The INVERSE color is used so triangles alternate white/black
-    display.fillTriangle(
-      display.width() / 2, display.height() / 2 - i,
-      display.width() / 2 - i, display.height() / 2 + i,
-      display.width() / 2 + i, display.height() / 2 + i, SSD1306_INVERSE);
-    display.display();
-    delay(1);
-  }
-
-  delay(2000);
-}
-
-void testdrawchar(void) {
-  display.clearDisplay();
-
-  display.setTextSize(1);               // Normal 1:1 pixel scale
-  display.setTextColor(SSD1306_WHITE);  // Draw white text
-  display.setCursor(0, 0);              // Start at top-left corner
-  display.cp437(true);                  // Use full 256 char 'Code Page 437' font
-
-  // Not all the characters will fit on the display. This is normal.
-  // Library will draw what it can and the rest will be clipped.
-  for (int16_t i = 0; i < 256; i++) {
-    if (i == '\n') display.write(' ');
-    else display.write(i);
-  }
-
-  display.display();
-  delay(2000);
-}
-
-void testdrawstyles(void) {
-  display.clearDisplay();
-
-  display.setTextSize(1);               // Normal 1:1 pixel scale
-  display.setTextColor(SSD1306_WHITE);  // Draw white text
-  display.setCursor(0, 0);              // Start at top-left corner
-  display.println(F("Hello, world!"));
-
-  display.setTextColor(SSD1306_BLACK, SSD1306_WHITE);  // Draw 'inverse' text
-  display.println(3.141592);
-
-  display.setTextSize(2);  // Draw 2X-scale text
-  display.setTextColor(SSD1306_WHITE);
-  display.print(F("0x"));
-  display.println(0xDEADBEEF, HEX);
-
-  display.display();
-  delay(2000);
-}
-
-void testscrolltext(void) {
-  display.clearDisplay();
-
-  display.setTextSize(2);  // Draw 2X-scale text
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(10, 0);
-  display.println(F("scroll"));
-  display.display();  // Show initial text
+  // [TODO] Read "Select" Button's state.
+  // If pressed -> Perform relevant action.
+  
   delay(100);
-
-  // Scroll in various directions, pausing in-between:
-  display.startscrollright(0x00, 0x0F);
-  delay(2000);
-  display.stopscroll();
-  delay(1000);
-  display.startscrollleft(0x00, 0x0F);
-  delay(2000);
-  display.stopscroll();
-  delay(1000);
-  display.startscrolldiagright(0x00, 0x07);
-  delay(2000);
-  display.startscrolldiagleft(0x00, 0x07);
-  delay(2000);
-  display.stopscroll();
-  delay(1000);
 }
 
-void testdrawbitmap(void) {
+
+// Clears Display -> Sets text size -> sets text color -> set cursor position -> Print -> call display()
+void first() {
   display.clearDisplay();
-
-  display.drawBitmap(
-    (display.width() - LOGO_WIDTH) / 2,
-    (display.height() - LOGO_HEIGHT) / 2,
-    logo_bmp, LOGO_WIDTH, LOGO_HEIGHT, 1);
+  display.setTextSize(3);
+  display.setTextColor(WHITE);  // Note: Color can be BLACK or WHITE. This is a hint for creating your "Menu" screen.
+  display.setCursor(15, 20);
+  display.println("Hello");
   display.display();
-  delay(1000);
-}
-
-#define XPOS 0  // Indexes into the 'icons' array in function below
-#define YPOS 1
-#define DELTAY 2
-
-void testanimate(const uint8_t *bitmap, uint8_t w, uint8_t h) {
-  int8_t f, icons[NUMFLAKES][3];
-
-  // Initialize 'snowflake' positions
-  for (f = 0; f < NUMFLAKES; f++) {
-    icons[f][XPOS] = random(1 - LOGO_WIDTH, display.width());
-    icons[f][YPOS] = -LOGO_HEIGHT;
-    icons[f][DELTAY] = random(1, 6);
-    Serial.print(F("x: "));
-    Serial.print(icons[f][XPOS], DEC);
-    Serial.print(F(" y: "));
-    Serial.print(icons[f][YPOS], DEC);
-    Serial.print(F(" dy: "));
-    Serial.println(icons[f][DELTAY], DEC);
-  }
-
-  for (;;) {                 // Loop forever...
-    display.clearDisplay();  // Clear the display buffer
-
-    // Draw each snowflake:
-    for (f = 0; f < NUMFLAKES; f++) {
-      display.drawBitmap(icons[f][XPOS], icons[f][YPOS], bitmap, w, h, SSD1306_WHITE);
-    }
-
-    display.display();  // Show the display buffer on the screen
-    delay(200);         // Pause for 1/10 second
-
-    // Then update coordinates of each flake...
-    for (f = 0; f < NUMFLAKES; f++) {
-      icons[f][YPOS] += icons[f][DELTAY];
-      // If snowflake is off the bottom of the screen...
-      if (icons[f][YPOS] >= display.height()) {
-        // Reinitialize to a random position, just off the top
-        icons[f][XPOS] = random(1 - LOGO_WIDTH, display.width());
-        icons[f][YPOS] = -LOGO_HEIGHT;
-        icons[f][DELTAY] = random(1, 6);
-      }
-    }
-  }
 }
---
+++ assignment-2-OLED/assignment-2-OLED.ino
@@ -0,0 +1,285 @@
+/**************************************************************************
+ This is an example for our Monochrome OLEDs based on SSD1306 drivers
+
+ Pick one up today in the adafruit shop!
+ ------> http://www.adafruit.com/category/63_98
+
+ This example is for a 128x64 pixel display using I2C to communicate
+ 3 pins are required to interface (two I2C and one reset).
+
+ Adafruit invests time and resources providing this open
+ source code, please support Adafruit and open-source
+ hardware by purchasing products from Adafruit!
+
+ Written by Limor Fried/Ladyada for Adafruit Industries,
+ with contributions from the open source community.
+ BSD license, check license.txt for more information
+ All text above, and the splash screen below must be
+ included in any redistribution.
+ **************************************************************************/
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128  // OLED display width, in pixels
+#define SCREEN_HEIGHT 64  // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+// The pins for I2C are defined by the Wire-library.
+// On an arduino UNO:       A4(SDA), A5(SCL)
+// On an arduino MEGA 2560: 20(SDA), 21(SCL)
+// On an arduino LEONARDO:   2(SDA),  3(SCL), ...
+#define OLED_RESET 4         // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3D  ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#define NUMFLAKES 10  // Number of snowflakes in the animation example
+
+#define LOGO_HEIGHT 16
+#define LOGO_WIDTH 16
+
+// Setup for buttons
+#define BUTTON_PIN_YELLOW 3
+#define BUTTON_PIN_RED 2
+int yellowButtonState = 0;
+int redButtonState = 0;
+
+// Current menu selection
+int menuSelection = 0;
+
+static const unsigned char PROGMEM logo_bmp[] = { 0b00000000, 0b11000000,
+                                                  0b00000001, 0b11000000,
+                                                  0b00000001, 0b11000000,
+                                                  0b00000011, 0b11100000,
+                                                  0b11110011, 0b11100000,
+                                                  0b11111110, 0b11111000,
+                                                  0b01111110, 0b11111111,
+                                                  0b00110011, 0b10011111,
+                                                  0b00011111, 0b11111100,
+                                                  0b00001101, 0b01110000,
+                                                  0b00011011, 0b10100000,
+                                                  0b00111111, 0b11100000,
+                                                  0b00111111, 0b11110000,
+                                                  0b01111100, 0b11110000,
+                                                  0b01110000, 0b01110000,
+                                                  0b00000000, 0b00110000 };
+
+void setup() {
+  Serial.begin(9600);
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;)
+      ;  // Don't proceed, loop forever
+  }
+
+  // Initialize button pins
+  pinMode(BUTTON_PIN_YELLOW, INPUT);
+  pinMode(BUTTON_PIN_RED, INPUT);
+
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  delay(2000);  // Pause for 2 seconds
+
+  // Clear the buffer
+  display.clearDisplay();
+
+  showHelloText();  // Show startup text
+
+  showMenu();                                        // Show the menu
+  display.fillRect(0, 0, 128, 16, SSD1306_INVERSE);  // Highlight first option
+  display.display();                                 // Show the highlight box
+
+  // testanimate(logo_bmp, LOGO_WIDTH, LOGO_HEIGHT);  // Animate bitmaps
+}
+
+void loop() {
+
+  yellowButtonState = digitalRead(BUTTON_PIN_YELLOW);
+  redButtonState = digitalRead(BUTTON_PIN_RED);
+
+  delay(100);
+
+  // Check if down-button is pressed and menu is switched
+  if (yellowButtonState == 1) {
+    menuSelection += 1;
+    if (menuSelection == 3) {
+      menuSelection = 0;  // if last position (2) is reached go back to first
+    }
+
+    display.clearDisplay();
+    showMenu();
+    switch (menuSelection) {
+      case 0:  // First option highlighted
+        display.fillRect(0, 0, 128, 16, SSD1306_INVERSE);
+        break;
+      case 1:  // Second option highlighted
+        display.fillRect(0, 16, 128, 17, SSD1306_INVERSE);
+        break;
+      case 2:  // Third option highlighted
+        display.fillRect(0, 33, 128, 17, SSD1306_INVERSE);
+        break;
+    }
+    display.display();
+  }
+
+  // Show each screen if Select-button is pressed
+  if (redButtonState == 1) {
+    switch (menuSelection) {
+      case 0:  // Open first option
+        showFirstScreen();
+        break;
+      case 1:  // Open second option
+        showSecondScreen();
+        break;
+      case 2:  // Open third option
+        display.clearDisplay();
+        display.display();
+        break;
+    }
+  }
+}
+
+void showHelloText(void) {
+  display.clearDisplay();
+  display.setTextSize(3);  // Draw 3X-scale text
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(10, 0);
+  display.println("Hello");
+  display.display();  // Show text on display
+  delay(1000);        // TODO change back to 4000
+}
+
+void showMenu(void) {
+  display.clearDisplay();
+  display.setTextSize(2);  // Draw 3X-scale text
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(10, 0);
+  display.println("1. Start");
+  display.setCursor(10, 17);
+  display.println("2. Stats");
+  display.setCursor(10, 34);
+  display.println("3. End");
+  display.display();  // Show text on display
+}
+
+void showFirstScreen(void) {
+  display.clearDisplay();
+  display.drawBitmap(
+    (display.width() - LOGO_WIDTH) / 2,
+    (display.height() - LOGO_HEIGHT) / 2,
+    logo_bmp, LOGO_WIDTH, LOGO_HEIGHT, 1);
+  display.display();
+}
+
+
+
+
+
+
+
+
+void testdrawstyles(void) {
+  display.clearDisplay();
+
+  display.setTextSize(1);               // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE);  // Draw white text
+  display.setCursor(0, 0);              // Start at top-left corner
+  display.println(F("Hello, world!"));
+
+  display.setTextColor(SSD1306_BLACK, SSD1306_WHITE);  // Draw 'inverse' text
+  display.println(3.141592);
+
+  display.setTextSize(2);  // Draw 2X-scale text
+  display.setTextColor(SSD1306_WHITE);
+  display.print(F("0x"));
+  display.println(0xDEADBEEF, HEX);
+
+  display.display();
+  delay(2000);
+}
+
+void showSecondScreen(void) {
+  display.clearDisplay();
+
+  display.setTextSize(2);  // Draw 2X-scale text
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(10, 0);
+  display.println(F("Sidra"));
+  display.setCursor(10, 17);
+  display.println(F("&"));
+  display.setCursor(10, 34);
+  display.println(F("Philipp"));
+  display.display();  // Show initial text
+  delay(100);
+
+  // Scroll in right and left, pausing in-between:
+  display.startscrollright(0x00, 0x0F);
+  delay(2000);
+  display.stopscroll();
+  delay(1000);
+  display.startscrollleft(0x00, 0x0F);
+  delay(2000);
+  display.stopscroll();
+  delay(1000);
+}
+
+void testdrawbitmap(void) {
+  display.clearDisplay();
+
+  display.drawBitmap(
+    (display.width() - LOGO_WIDTH) / 2,
+    (display.height() - LOGO_HEIGHT) / 2,
+    logo_bmp, LOGO_WIDTH, LOGO_HEIGHT, 1);
+  display.display();
+  delay(1000);
+}
+
+#define XPOS 0  // Indexes into the 'icons' array in function below
+#define YPOS 1
+#define DELTAY 2
+
+void testanimate(const uint8_t *bitmap, uint8_t w, uint8_t h) {
+  int8_t f, icons[NUMFLAKES][3];
+
+  // Initialize 'snowflake' positions
+  for (f = 0; f < NUMFLAKES; f++) {
+    icons[f][XPOS] = random(1 - LOGO_WIDTH, display.width());
+    icons[f][YPOS] = -LOGO_HEIGHT;
+    icons[f][DELTAY] = random(1, 6);
+    Serial.print(F("x: "));
+    Serial.print(icons[f][XPOS], DEC);
+    Serial.print(F(" y: "));
+    Serial.print(icons[f][YPOS], DEC);
+    Serial.print(F(" dy: "));
+    Serial.println(icons[f][DELTAY], DEC);
+  }
+
+  for (;;) {                 // Loop forever...
+    display.clearDisplay();  // Clear the display buffer
+
+    // Draw each snowflake:
+    for (f = 0; f < NUMFLAKES; f++) {
+      display.drawBitmap(icons[f][XPOS], icons[f][YPOS], bitmap, w, h, SSD1306_WHITE);
+    }
+
+    display.display();  // Show the display buffer on the screen
+    delay(200);         // Pause for 1/10 second
+
+    // Then update coordinates of each flake...
+    for (f = 0; f < NUMFLAKES; f++) {
+      icons[f][YPOS] += icons[f][DELTAY];
+      // If snowflake is off the bottom of the screen...
+      if (icons[f][YPOS] >= display.height()) {
+        // Reinitialize to a random position, just off the top
+        icons[f][XPOS] = random(1 - LOGO_WIDTH, display.width());
+        icons[f][YPOS] = -LOGO_HEIGHT;
+        icons[f][DELTAY] = random(1, 6);
+      }
+    }
+  }
+}
