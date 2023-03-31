09a5e692f1401d9b688e6265313d9ee9e05da728
phlpp77 <PhilippHemkemeyer@iCloud.com>
2023-03-31 16:08:44 -0400

    Fix bug white screen

--- planet-protectors-game/planet-protectors-game.ino
+++ planet-protectors-game/planet-protectors-game.ino
@@ -37,23 +37,23 @@
 #include <Adafruit_ST7789.h>  // Hardware-specific library for ST7789
 #include <SPI.h>
 
-#if defined(ARDUINO_FEATHER_ESP32)  // Feather Huzzah32
-#define TFT_CS 14
-#define TFT_RST 15
-#define TFT_DC 32
+// #if defined(ARDUINO_FEATHER_ESP32)  // Feather Huzzah32
+// #define TFT_CS 14
+// #define TFT_RST 15
+// #define TFT_DC 32
 
-#elif defined(ESP8266)
-#define TFT_CS 4
-#define TFT_RST 16
-#define TFT_DC 5
+// #elif defined(ESP8266)
+// #define TFT_CS 4
+// #define TFT_RST 16
+// #define TFT_DC 5
 
-#else
+// #else
 // For the breakout board, you can use any 2 or 3 pins.
 // These pins will also work for the 1.8" TFT shield.
 #define TFT_CS 10
 #define TFT_RST 9  // Or set to -1 and connect to Arduino RESET pin
 #define TFT_DC 8
-#endif
+// #endif
 
 // OPTION 1 (recommended) is to use the HARDWARE SPI pins, which are unique
 // to each board and not reassignable. For Arduino Uno: MOSI = pin 11 and
