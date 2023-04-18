dfe79eab56892cf02034418665f9c1716cd80ab9
phlpp77 <PhilippHemkemeyer@iCloud.com>
2023-04-17 22:24:09 -0400

    Change to Arduino Mega, Works on final prototype

--- planet-protectors-game/planet-protectors-game.ino
+++ planet-protectors-game/planet-protectors-game.ino
@@ -29,9 +29,9 @@
 #include <SPI.h>
 
 // These pins will also work for the 1.8" TFT shield.
-#define TFT_CS 10
-#define TFT_RST 9  // Or set to -1 and connect to Arduino RESET pin
-#define TFT_DC 8
+#define TFT_CS 45   //10 //45
+#define TFT_RST -1  //9  //43 // Or set to -1 and connect to Arduino RESET pin
+#define TFT_DC 41   //8 // 41
 
 // For 1.44" and 1.8" TFT with ST7735 use:
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
@@ -52,10 +52,10 @@ bool state;
 /* Light sensor setup */
 
 // Set analog pins to receive the lightsensor data
-#define recyclingStationPin A0
-#define landfillStationPin A1
-#define compostStationPin A2
-#define charmStationPin A3
+#define recyclingStationPin A3
+#define landfillStationPin A2
+#define compostStationPin A1
+#define charmStationPin A0
 
 // Initialize value variables
 int recyclingStationValue = 0;
@@ -63,7 +63,7 @@ int landfillStationValue = 0;
 int compostStationValue = 0;
 int charmStationValue = 0;
 
-int lightlevel = 45;
+int lightlevel = 150;
 
 /* LED setup */
 #define redLED 7
@@ -93,30 +93,27 @@ public:
   }
 };
 
-#define PRODUCT_COUNT 9  // Array lengeth - amount of products we use
+
+
+
+#define PRODUCT_COUNT 14  // Array lengeth - amount of products we use
 
 // Array of products that are used with id being the RFID-tag number
-Product products[] = {
-  Product("4224317959177", "Milk", 1, "This item is made from paper and plastic", "This item can be used again"),
-  Product("13205115110472", "Milk", 1, "This item is made from paper and plastic", "This item can be used again"),
-  Product("13205111514375", "Milk", 1, "This item is made from paper and plastic", "This item can be used again"),
-  Product("42243114578", "Milk", 1, "This item is made from paper and plastic", "This item can be used again"),
-  Product("5322812843128", "Milk", 1, "Tf", "t"),
-  Product("5322812843128", "Milk", 1, "ert", "Tfs"),
-  Product("5322812843128", "Milk", 1, "T3", "23"),
-  Product("5322812843128", "Milk", 1, "12", "re"),
-  Product("5322812843128", "Milk", 1, "T12", "d"),
-  // Product("13205115110472", "Strawberry Jam", 2, "This item is made from paper and plastic", "This item can be used again"),
-  // Product("13205111514375", "Cereal", 1, "This item is made from cardboard", "This item can be used again when correclty disposed of"),
-  // Product("42243114578", "Peanut butter", 1, "This item is made from plastic", "This item can be used again when correclty disposed of"),
-  // Product("5322812843128", "Battery", 4, "This item contains hazardus waste", "This item is hard to recycle "),
-  // Product("5322815818413","Granola bar wrapper",1,"This item is made from plastic","This item can NOT be used again when correclty disposed of"),
-  // Product("422431138630","Coffee Cup",1,"This item is made from paper and plastic","This item can be used again when correclty disposed of"),
-  // Product("42244133105210","Soda can",1,"This item is made from aluminum","This item can be used again when correclty disposed of"),
-  //Product("421210235163139","Apple",3,"This item is grown from the ground","This item can decompose easily (when put back in the ground/when broken down?)"),
-  // Product("","",,"",""),
-  // Product("","",,"",""),
-  // Product("","",,"",""),
+Product products[PRODUCT_COUNT] = {
+  Product("4224317959177", "Milk", 1, "This item is made \nfrom paper\nand \nplastic", "This item can be \nused again\nwhen cor-\nrectly \ndisposed"),
+  Product("13205111514375", "Cereal", 1, "This item is made from cardboard", "This item can be \nused again\nwhen cor-\nrectly \ndisposed"),
+  Product("13205115110472", "Straw- \n berry Jam", 1, "This item is made from paper and \n plastic", "This item can be \nused again\nwhen cor-\nrectly \ndisposed"),
+  Product("42243114578", "Peanut \n Butter", 1, "This item is made from plastic", "This item can be \nused again\nwhen cor-\nrectly \ndisposed"),
+  Product("5322812843128", "Battery", 4, "This item contains \nhazardus \nwaste", "This item is hard to\nrecycle"),
+  Product("422431138630", "Coffee \n Cup", 1, "This item is made from paper and plastic", "This item can be \nused again\nwhen cor-\nrectly \ndisposed"),
+  Product("42244133105210", "Soda can", 1, "This item is made from aluminum", "This item can be \nused again\nwhen cor-\nrectly \ndisposed"),
+  Product("5322815818413", "Granolabar \n Wrapper", 2, "This item is made from plastic", "This item can NOT be used again when correclty disposed"),
+  Product("421210235163139", "Apple", 3, "This item is grown\nfrom the\nground", "This item can decompose easily\n(when put\nback on the ground)"),
+  Product("5322816722282", "Banana", 3, "This item is grown\nfrom the\nground", "This item can decompose easily\n(when put\nback on the ground)"),
+  Product("132051129186140", "Plastic \n Fork", 2, "This item is made from plastic", "This item can NOT be used again when correclty disposed"),
+  Product("1320511531432", "Light \n Bulb", 4, "This item contains \nhazardus \nwaste", "This item is hard to\nrecycle"),
+  Product("4224315212932", "Old \n Phone", 4, "This item contains \nhazardus \nwaste", "This item is hard to\nrecycle"),
+  Product("5322813062151", "Paint \n Bucket", 4, "This item contains \n hazardus \n waste", "This item is hard to\nrecycle"),
 };
 
 /* Game logic variables */
@@ -127,12 +124,11 @@ int overallPoints = 0;
 
 /* Setup when Arudino starts */
 void setup(void) {
-  Serial.begin(9600);         // Start serial connection to computer for print etc.
-  tft.initR(INITR_BLACKTAB);  // Init ST7735S chip, black tab
+  Serial.begin(9600);            // Start serial connection to computer for print etc.
+  tft.initR(INITR_144GREENTAB);  // Init ST7735S chip - screen
+  tft.setRotation(1);            // Rotate the screen by 90º to fit the mountain
 
   // Start animation
-  testtriangles();  // Startup animation
-  delay(500);
   startUpScreen();  // Show the logo at start
 
   // Setup of sensors and lights
@@ -142,6 +138,7 @@ void setup(void) {
 
   // Start with game
   clearDisplay();  // Clears the startup image from the screen
+  tft.print("Test");
   showScanItem();  // Start with the interactions
 }
 
@@ -150,14 +147,11 @@ void setup(void) {
 void loop() {
 
   String scannerValue = myRfid.getTag();  // Read scanner data
-  //Serial.print("Scanner: ");
-  //Serial.println(scannerValue);
-  // Serial.println(products[PRODUCT_COUNT].id);
 
   delay(250);
 
   // Saves active product into the currentTag variable
-  if (scannerValue != "00000") {  // Check if tag is on scanner
+  if (scannerValue != "000000") {  // Check if tag is on scanner
     currentTag = scannerValue;     // Save value to current Tag
     Serial.print("Tag detected: ");
     Serial.println(currentTag);
@@ -181,7 +175,7 @@ void loop() {
       Serial.println("Nope!");
     }
   }
-  /*
+
   readLightSensorValues();  // Read the light sensors at the stations
 
   // Checking stations
@@ -203,7 +197,7 @@ void loop() {
   if (charmStationValue <= lightlevel) {  // Check for item at charm station
     Serial.println("Item found on charm station");
     checkCorrect(productIndex, 4);  // Check if the item is correctly disposed at charm station
-  } */
+  }
 }
 
 
@@ -292,6 +286,12 @@ void setupLightSensors() {
   pinMode(landfillStationPin, INPUT);
   pinMode(compostStationPin, INPUT);
   pinMode(charmStationPin, INPUT);
+  readLightSensorValues();
+  int lightLevelSum = recyclingStationValue + landfillStationValue + compostStationValue + charmStationValue;
+  int averageLightLevel = lightLevelSum / 4;
+  lightlevel = averageLightLevel - 50;
+  Serial.print("Light level adapted to environment: ");
+  Serial.println(lightlevel);
 }
 
 /* Initialize LEDs */
@@ -302,8 +302,19 @@ void setupLEDs() {
 
 /* Show the startup screen logo */
 void startUpScreen() {
-  // show bitmap logo here
-  // delay(4000);
+  startUpAnimation();  // Startup animation
+  delay(500);
+  tft.setCursor(10, 10);
+  tft.setTextSize(3);
+  tft.setTextColor(0x001F);
+  tft.print("PLANET");
+  tft.setCursor(10, 40);
+  tft.setTextColor(0xF800);
+  tft.print("PROTEC");
+  tft.setCursor(10, 65);
+  tft.print("TORS");
+
+  delay(3000);
 }
 
 
@@ -312,7 +323,13 @@ void startUpScreen() {
 /* Show text to scan item */
 void showScanItem() {
   clearDisplay();
-  showText("Scan item at home");
+  showText("Scan item \n at home");
+}
+
+/* Show text to scan new item */
+void showScanNewItem() {
+  clearDisplay();
+  showText("Scan new \n item at \n home");
 }
 
 /* Show that decision was correct */
@@ -350,14 +367,14 @@ void showCorrect(int points) {
   tft.print("points:");
   tft.print(overallPoints);
 
-  while (recyclingStationValue <= lightlevel || landfillStationValue <= lightlevel) {  // Show points as long as item is placed
+  while (recyclingStationValue <= lightlevel || landfillStationValue <= lightlevel || compostStationValue <= lightlevel || charmStationValue <= lightlevel) {  // Show points as long as item is placed
     delay(1000);
     readLightSensorValues();
   }
 
   delay(1000);
   clearDisplay();
-  showScanItem();
+  showScanNewItem();
 }
 
 /* Show that decision was wrong */
@@ -380,10 +397,11 @@ void showWrong(bool firstTry, int product) {
   }
   Serial.print("Print hint: ");
   Serial.println(text);
+  tft.setCursor(0, 5);
   tft.println("Hint: ");
   tft.print(text);
 
-  while (recyclingStationValue <= lightlevel || landfillStationValue <= lightlevel) {  // Show hint as long as item is on statiion
+  while (recyclingStationValue <= lightlevel || landfillStationValue <= lightlevel || compostStationValue <= lightlevel || charmStationValue <= lightlevel) {  // Show hint as long as item is on statiion
     delay(1000);
     readLightSensorValues();
   }
@@ -415,14 +433,14 @@ void showFinallyWrong() {
   tft.setCursor(10, 70);
   tft.print("correct.");
 
-  while (recyclingStationValue <= lightlevel || landfillStationValue <= lightlevel) {  // Show message as long as item is on statiion
+  while (recyclingStationValue <= lightlevel || landfillStationValue <= lightlevel || compostStationValue <= lightlevel || charmStationValue <= lightlevel) {  // Show message as long as item is on statiion
     delay(1000);
     readLightSensorValues();
   }
   delay(1000);
   digitalWrite(redLED, LOW);
   clearDisplay();
-  showScanItem();
+  showScanNewItem();
 }
 
 
@@ -442,193 +460,23 @@ void showText(char *text) {
   tft.print(text);
 }
 
-
-
-// void testlines(uint16_t color) {
-//   tft.fillScreen(ST77XX_BLACK);
-//   for (int16_t x = 0; x < tft.width(); x += 6) {
-//     tft.drawLine(0, 0, x, tft.height() - 1, color);
-//     delay(0);
-//   }
-//   for (int16_t y = 0; y < tft.height(); y += 6) {
-//     tft.drawLine(0, 0, tft.width() - 1, y, color);
-//     delay(0);
-//   }
-
-//   tft.fillScreen(ST77XX_BLACK);
-//   for (int16_t x = 0; x < tft.width(); x += 6) {
-//     tft.drawLine(tft.width() - 1, 0, x, tft.height() - 1, color);
-//     delay(0);
-//   }
-//   for (int16_t y = 0; y < tft.height(); y += 6) {
-//     tft.drawLine(tft.width() - 1, 0, 0, y, color);
-//     delay(0);
-//   }
-
-//   tft.fillScreen(ST77XX_BLACK);
-//   for (int16_t x = 0; x < tft.width(); x += 6) {
-//     tft.drawLine(0, tft.height() - 1, x, 0, color);
-//     delay(0);
-//   }
-//   for (int16_t y = 0; y < tft.height(); y += 6) {
-//     tft.drawLine(0, tft.height() - 1, tft.width() - 1, y, color);
-//     delay(0);
-//   }
-
-//   tft.fillScreen(ST77XX_BLACK);
-//   for (int16_t x = 0; x < tft.width(); x += 6) {
-//     tft.drawLine(tft.width() - 1, tft.height() - 1, x, 0, color);
-//     delay(0);
-//   }
-//   for (int16_t y = 0; y < tft.height(); y += 6) {
-//     tft.drawLine(tft.width() - 1, tft.height() - 1, 0, y, color);
-//     delay(0);
-//   }
-// }
-
-// void testdrawtext(char *text, uint16_t color) {
-//   tft.setCursor(0, 0);
-//   tft.setTextColor(color);
-//   tft.setTextWrap(true);
-//   tft.print(text);
-// }
-
-// void testfastlines(uint16_t color1, uint16_t color2) {
-//   tft.fillScreen(ST77XX_BLACK);
-//   for (int16_t y = 0; y < tft.height(); y += 5) {
-//     tft.drawFastHLine(0, y, tft.width(), color1);
-//   }
-//   for (int16_t x = 0; x < tft.width(); x += 5) {
-//     tft.drawFastVLine(x, 0, tft.height(), color2);
-//   }
-// }
-
-// void testdrawrects(uint16_t color) {
-//   tft.fillScreen(ST77XX_BLACK);
-//   for (int16_t x = 0; x < tft.width(); x += 6) {
-//     tft.drawRect(tft.width() / 2 - x / 2, tft.height() / 2 - x / 2, x, x, color);
-//   }
-// }
-
-// void testfillrects(uint16_t color1, uint16_t color2) {
-//   tft.fillScreen(ST77XX_BLACK);
-//   for (int16_t x = tft.width() - 1; x > 6; x -= 6) {
-//     tft.fillRect(tft.width() / 2 - x / 2, tft.height() / 2 - x / 2, x, x, color1);
-//     tft.drawRect(tft.width() / 2 - x / 2, tft.height() / 2 - x / 2, x, x, color2);
-//   }
-// }
-
-// void testfillcircles(uint8_t radius, uint16_t color) {
-//   for (int16_t x = radius; x < tft.width(); x += radius * 2) {
-//     for (int16_t y = radius; y < tft.height(); y += radius * 2) {
-//       tft.fillCircle(x, y, radius, color);
-//     }
-//   }
-// }
-
-// void testdrawcircles(uint8_t radius, uint16_t color) {
-//   for (int16_t x = 0; x < tft.width() + radius; x += radius * 2) {
-//     for (int16_t y = 0; y < tft.height() + radius; y += radius * 2) {
-//       tft.drawCircle(x, y, radius, color);
-//     }
-//   }
-// }
-
-void testtriangles() {
-  tft.fillScreen(ST77XX_BLACK);
-  uint16_t color = 0xF800;
-  int t;
-  int w = tft.width() / 2;
-  int x = tft.height() - 1;
-  int y = 0;
-  int z = tft.width();
-  for (t = 0; t <= 15; t++) {
-    tft.drawTriangle(w, y, y, x, z, x, color);
-    x -= 4;
-    y += 4;
-    z -= 4;
-    color += 100;
+void startUpAnimation() {
+  for (int i = 0; i <= 3; i++) {
+    tft.fillScreen(ST77XX_BLACK);
+    uint16_t color = 0xF800;
+    int t;
+    int w = tft.width() / 2;
+    int x = tft.height() - 1;
+    int y = 0;
+    int z = tft.width();
+    for (t = 0; t <= 15; t++) {
+      tft.drawTriangle(w, y, y, x, z, x, color);
+      x -= 4;
+      y += 4;
+      z -= 4;
+      color += 100;
+      delay(10);
+    }
+    clearDisplay();
   }
 }
-
-// void testroundrects() {
-//   tft.fillScreen(ST77XX_BLACK);
-//   uint16_t color = 100;
-//   int i;
-//   int t;
-//   for (t = 0; t <= 4; t += 1) {
-//     int x = 0;
-//     int y = 0;
-//     int w = tft.width() - 2;
-//     int h = tft.height() - 2;
-//     for (i = 0; i <= 16; i += 1) {
-//       tft.drawRoundRect(x, y, w, h, 5, color);
-//       x += 2;
-//       y += 3;
-//       w -= 4;
-//       h -= 6;
-//       color += 1100;
-//     }
-//     color += 100;
-//   }
-// }
-
-// void tftPrintTest() {
-//   tft.setTextWrap(false);
-//   tft.fillScreen(ST77XX_BLACK);
-//   tft.setCursor(0, 30);
-//   tft.setTextColor(ST77XX_RED);
-//   tft.setTextSize(1);
-//   tft.println("Hello World!");
-//   tft.setTextColor(ST77XX_YELLOW);
-//   tft.setTextSize(2);
-//   tft.println("Hello World!");
-//   tft.setTextColor(ST77XX_GREEN);
-//   tft.setTextSize(3);
-//   tft.println("Hello World!");
-//   tft.setTextColor(ST77XX_BLUE);
-//   tft.setTextSize(4);
-//   tft.print(1234.567);
-//   delay(1500);
-//   tft.setCursor(0, 0);
-//   tft.fillScreen(ST77XX_BLACK);
-//   tft.setTextColor(ST77XX_WHITE);
-//   tft.setTextSize(0);
-//   tft.println("Hello World!");
-//   tft.setTextSize(1);
-//   tft.setTextColor(ST77XX_GREEN);
-//   // tft.print(p, 6);
-//   tft.println(" Want pi?");
-//   tft.println(" ");
-//   tft.print(8675309, HEX);  // print 8,675,309 out in HEX!
-//   tft.println(" Print HEX!");
-//   tft.println(" ");
-//   tft.setTextColor(ST77XX_WHITE);
-//   tft.println("Sketch has been");
-//   tft.println("running for: ");
-//   tft.setTextColor(ST77XX_MAGENTA);
-//   tft.print(millis() / 1000);
-//   tft.setTextColor(ST77XX_WHITE);
-//   tft.print(" seconds.");
-// }
-
-// void mediabuttons() {
-//   // play
-//   tft.fillScreen(ST77XX_BLACK);
-//   tft.fillRoundRect(25, 10, 78, 60, 8, ST77XX_WHITE);
-//   tft.fillTriangle(42, 20, 42, 60, 90, 40, ST77XX_RED);
-//   delay(500);
-//   // pause
-//   tft.fillRoundRect(25, 90, 78, 60, 8, ST77XX_WHITE);
-//   tft.fillRoundRect(39, 98, 20, 45, 5, ST77XX_GREEN);
-//   tft.fillRoundRect(69, 98, 20, 45, 5, ST77XX_GREEN);
-//   delay(500);
-//   // play color
-//   tft.fillTriangle(42, 20, 42, 60, 90, 40, ST77XX_BLUE);
-//   delay(50);
-//   // pause color
-//   tft.fillRoundRect(39, 98, 20, 45, 5, ST77XX_RED);
-//   tft.fillRoundRect(69, 98, 20, 45, 5, ST77XX_RED);
-//   // play color
-//   tft.fillTriangle(42, 20, 42, 60, 90, 40, ST77XX_GREEN);
-// }
